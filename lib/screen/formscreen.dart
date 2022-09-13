911a94abfadad727522d473b628c428868da1530
Shuttoku <108716387+Shuttoku@users.noreply.github.com>
2022-09-13 12:43:49 +0700

    hi2

---
+++ lib/screen/formscreen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class FormScreen extends StatefulWidget {
+  const FormScreen({Key? key}) : super(key: key);
+
+  @override
+  State<FormScreen> createState() => _FormScreenState();
+}
+
+class _FormScreenState extends State<FormScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("แบบฟอร์ม"),),
+    );
+    
+  }
+}
